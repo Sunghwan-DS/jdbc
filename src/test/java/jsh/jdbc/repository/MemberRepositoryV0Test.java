02161ed5467a14b1a0063f518fd7973c5a4429f4
Sunghwan-DS <jeonsung02@gmail.com>
2024-03-15 21:04:54 +0900

    Add inquiry member method in MemberRepository

--- src/test/java/jsh/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/jsh/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,17 +1,27 @@
 package jsh.jdbc.repository;
 
 import jsh.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
 public class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repositoryV0 = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
+        //save
         Member member = new Member("memberV0", 10000);
         repositoryV0.save(member);
+
+        //findById
+        Member findMember = repositoryV0.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 }
