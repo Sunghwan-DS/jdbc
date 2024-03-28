a45e4e4d85286dd2274cdc67583b2b3ab4bafe51
Sunghwan-DS <jeonsung02@gmail.com>
2024-03-28 22:33:35 +0900

    Copy MemberServiceV1 to MemberServiceV2 for using Connection Param

---
+++ src/main/java/jsh/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,31 @@
+package jsh.jdbc.service;
+
+import jsh.jdbc.domain.Member;
+import jsh.jdbc.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 파라미터 연동, 풀을 고려한 종료
+ */
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final MemberRepositoryV2 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
